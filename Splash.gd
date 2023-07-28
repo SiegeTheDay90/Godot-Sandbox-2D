a1f2c8c7035ad7440cb80bf3df6af123929f46fa
cjsmi <ClarenceSmith90@gmail.com>
2023-07-27 23:03:00 -0400

    add lighting demo with PointLight2D

--- Splash.gd
+++ Splash.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://test_level_1/test_level_1.tscn")
+	get_tree().change_scene_to_file("res://level_1_platforms_test/test_level_1.tscn")
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://test_level_2/Test_Level_2.tscn")
+	get_tree().change_scene_to_file("res://level_2_physics_polygon_test/Test_Level_2.tscn")
+
+
+func _on_level_3_pressed():
+	get_tree().change_scene_to_file("res://level_3_lighting_test/test_level_3.tscn")
--- level_2_physics_polygon_test/RigidBody2D.gd
+++ level_2_physics_polygon_test/RigidBody2D.gd
@@ -2,8 +2,6 @@ extends RigidBody2D
 
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-print(gravity)
 
-
-func _process(delta):
+func _physics_process(delta):
 	position.y += gravity * delta
